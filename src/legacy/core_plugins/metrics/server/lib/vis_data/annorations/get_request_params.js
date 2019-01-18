ecf5af769dd4d472a47dfe6f450fae090b9ac59a
Alexey Antonov <alexwizp@gmail.com>
2019-01-18 14:04:10 +0300

    apply search strategy for annotations request

--- src/legacy/core_plugins/metrics/server/lib/vis_data/annorations/get_request_params.js
+++ src/legacy/core_plugins/metrics/server/lib/vis_data/annorations/get_request_params.js
@@ -16,12 +16,24 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+import buildRequestBody from './build_request_body';
 
-import buildProcessorFunction from './build_processor_function';
-import processors from './request_processors/annotations';
+export default (req, panel, annotation, isBatchRequest = true) => {
+  const bodies = [];
 
-export default function buildAnnotationRequest(req, panel, annotation) {
-  const processor = buildProcessorFunction(processors, req, panel, annotation);
-  const doc = processor({});
-  return doc;
-}
+  if (isBatchRequest) {
+    const indexPattern = annotation.index_pattern;
+
+    bodies.push({
+      index: indexPattern,
+      ignoreUnavailable: true,
+    });
+  }
+
+  bodies.push({
+    ...buildRequestBody(req, panel, annotation),
+    timeout: '90s'
+  });
+
+  return bodies;
+};
